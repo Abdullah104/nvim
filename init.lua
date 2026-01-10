b7fac49d56c5600250a3f7d4533b92788260bc35
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2026-01-10 16:06:19 +0300

    add Telescope

--- init.lua
+++ init.lua
@@ -8,3 +8,16 @@ local vertical_movements = { 'j', 'k', 'G', 'gg', '<C-d>', '<C-u>' }
 for _, key in ipairs(vertical_movements) do
 	vim.keymap.set('n', key, key .. 'zz', { noremap = true })
 end
+
+-- vim-plug
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope-fzf-native.nvim', { ['do'] = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install' })
+Plug('nvim-tree/nvim-web-devicons')
+Plug('nvim-telescope/telescope.nvim')
+
+vim.call('plug#end')
