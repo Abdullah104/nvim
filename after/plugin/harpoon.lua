4ad908c6b6ff9a76b5041adb5a07d981bbbeb8dd
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2026-01-23 18:02:14 +0300

    harpoon

---
+++ after/plugin/harpoon.lua
@@ -0,0 +1,48 @@
+local harpoon = require('harpoon')
+
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>a", function()
+   harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+   {
+      desc = 'harpoon menu'
+   }
+)
+
+-- basic telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+   local file_paths = {}
+   for _, item in ipairs(harpoon_files.items) do
+      table.insert(file_paths, item.value)
+   end
+
+   require("telescope.pickers").new({}, {
+      prompt_title = "Harpoon",
+      finder = require("telescope.finders").new_table({
+         results = file_paths,
+      }),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+   }):find()
+end
+
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, {
+desc = "Open harpoon window"
+})
+
+for i = 1, 9 do
+   vim.keymap.set('n', '<leader>'..i, function() harpoon:list():select(i) end, {
+   desc = 'Open harpoon item #'..i
+})
+end
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, {
+   desc = 'harpoon next'
+})
+
+vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end, {
+   desc = 'harpoon next'
+})
+
--- lua/plug.lua
+++ lua/plug.lua
@@ -5,12 +5,14 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 
 Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope-fzf-native.nvim',
-    { ['do'] = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install' })
+Plug('nvim-telescope/telescope-fzf-native.nvim', {
+   ['do'] = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
+})
 Plug('nvim-tree/nvim-web-devicons')
 Plug('nvim-telescope/telescope.nvim')
 Plug('projekt0n/github-nvim-theme')
 Plug('nvim-treesitter/nvim-treesitter', { ['run'] = ':TSUpdate' })
+Plug('ThePrimeagen/harpoon', { branch = 'harpoon2', dependencies = 'nvim-lua/plenary.nvim' })
 
 vim.call('plug#end')
 
