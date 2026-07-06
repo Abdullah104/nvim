c6de9a505f01120dd84ffcf0c06b5f6056853412
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2026-07-06 15:44:08 +0300

    Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Insert new line below and stay in insert mode
+-- Insert new empty line below and stay in insert mode
 vim.keymap.set("i", "<C-CR>", "<Esc>o", { noremap = true })
+
+-- Insert new empty line above and stay in insert mode
+vim.keymap.set("i", "<CA-CR>", "<Esc>O", { noremap = true })
+
+-- Delete the current line
+vim.keymap.set("i", "<C-d>", "<Esc>Vd", { noremap = true })
